fcb5161ede926fcffb25e78be7b72a9f83f445da
missahng <awr0607@gmail.com>
2016-09-13 11:29:49 +0000

    edit profile exception

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,13 +8,23 @@ class ArtistsController < ApplicationController
         @anothers = Market.where(artist_id: @artist)
         
         #작가 프로필
-        @profile = Profile.find_by(artistid: @artist.id)
-        @academics = @profile.academic.split(/\r\n/)
-        @awards = @profile.awards.split(/\r\n/)
-        @privates = @profile.privateexhibitions.split(/\r\n/)
-        @teams = @profile.teamexhibitions.split(/\r\n/)
-        @fairs = @profile.artfairs.split(/\r\n/)
-        @possessions = @profile.possessions.split(/\r\n/)
-        @etc = @profile.etc.split(/\r\n/)
+        @profile = Profile.find_by(artistid: @market.artist.id)
+        if @profile != nil
+          @academics = @profile.academic.split(/\r\n/)
+          @awards = @profile.awards.split(/\r\n/)
+          @privates = @profile.privateexhibitions.split(/\r\n/)
+          @teams = @profile.teamexhibitions.split(/\r\n/)
+          @fairs = @profile.artfairs.split(/\r\n/)
+          @possessions = @profile.possessions.split(/\r\n/)
+          @etc = @profile.etc.split(/\r\n/)
+        else
+          @academics = "미등록"
+          @awards = "미등록"
+          @privates = "미등록"
+          @teams = "미등록"
+          @fairs = "미등록"
+          @possessions = "미등록"
+          @etc = "미등록"
+        end
     end
 end
--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -71,13 +71,23 @@ class MarketsController < ApplicationController
     @artist = Artist.find(@market.artist)
     #작가 프로필
     @profile = Profile.find_by(artistid: @market.artist.id)
-    @academics = @profile.academic.split(/\r\n/)
-    @awards = @profile.awards.split(/\r\n/)
-    @privates = @profile.privateexhibitions.split(/\r\n/)
-    @teams = @profile.teamexhibitions.split(/\r\n/)
-    @fairs = @profile.artfairs.split(/\r\n/)
-    @possessions = @profile.possessions.split(/\r\n/)
-    @etc = @profile.etc.split(/\r\n/)
+    if @profile != nil
+      @academics = @profile.academic.split(/\r\n/)
+      @awards = @profile.awards.split(/\r\n/)
+      @privates = @profile.privateexhibitions.split(/\r\n/)
+      @teams = @profile.teamexhibitions.split(/\r\n/)
+      @fairs = @profile.artfairs.split(/\r\n/)
+      @possessions = @profile.possessions.split(/\r\n/)
+      @etc = @profile.etc.split(/\r\n/)
+    else
+      @academics = "미등록"
+      @awards = "미등록"
+      @privates = "미등록"
+      @teams = "미등록"
+      @fairs = "미등록"
+      @possessions = "미등록"
+      @etc = "미등록"
+    end
 
   end
   
