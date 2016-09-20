7fe1d211f019a1346a4f2a21dd7d1781818c3cc8
missahng <awr0607@gmail.com>
2016-09-20 14:05:19 +0000

    edit artists error

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -70,9 +70,7 @@ class MarketsController < ApplicationController
     #작가 이름 
     @artist = Artist.find(@market.artist)
     #작가 프로필
-    #@profile = Profile.find_by(artistid: @market.artist.id)
-    #artist와 user아이디의 불일치로 생기는 문제로 인해 잠시 변경
-    @profile = Profile.find_by(artistid: @market.artist.user_id)
+    @profile = Profile.find_by(artistid: @market.artist.id)
     unless @profile.nil?
       @academics = @profile.academic.split(/\r\n/)
       @awards = @profile.awards.split(/\r\n/)
